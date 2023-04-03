cc8f24a7d4543ec52775e5e47ae87a28a3c65584
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-03 19:38:08 +0200

    Add effect for setting isLoaded when image of card has loaded

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
 import '../styles/Card.css';
 
 const Card = (props) => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
+    useEffect(() => {
+        const image = new Image();
+        image.src = props.image;
+        image.onload = () => {
+            setImageLoaded(true);
+        }
+    }, []);
+
     return (
         <div className="card">
-            <img className="card-image" src={props.image} alt="props.alt"></img>
+            {imageLoaded ? 
+                <img className="card-image" src={props.image} alt="props.alt"></img>
+            :
+            <div>Loading...</div>
+            }
             <div className="card-text">{props.text}</div>
         </div>
     )
