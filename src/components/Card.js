663fc32de31759cce140fe94c003735309d1e21e
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-03 19:11:14 +0200

    Fix Card image prop not using brackets

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import '../styles/Card.css';
 const Card = (props) => {
     return (
         <div className="card">
-            <img className="card-image" src="props.image" alt="props.alt"></img>
+            <img className="card-image" src={props.image} alt="props.alt"></img>
             <div className="card-text">{props.text}</div>
         </div>
     )
