254d327f62eb3c3008fa445ca5226c32a4c7582f
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-04 18:15:01 +0200

    Add CardManager css file

--- src/components/CardManager.js
+++ src/components/CardManager.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
+import '../styles/CardManager.css';
 
 const CardManager = (props) => {
     const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        setCards(props.cards);
+    }, [])
     return (
         <div className="card-manager">
             {cards.map((card) => {
                 return (
                     <Card 
+                    key={card.image}
                     image={card.image}
                     alt={card.alt}
                     text={card.text}
