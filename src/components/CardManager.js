f84f44d22f6ed212bbeb4eee278ef70742483740
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-04 20:40:11 +0200

    Add getCardsRandom function for getting cards in random order

--- src/components/CardManager.js
+++ src/components/CardManager.js
@@ -5,16 +5,30 @@ import '../styles/CardManager.css';
 const CardManager = (props) => {
     const [cards, setCards] = useState([]);
 
-    useEffect(() => {
-        setCards(props.cards);
-    }, [])
-
     const onCardClick = (id) => {
         console.log('click' + id);
     }
+
+    const getCardsRandom = () => {
+        const tempCards = [...props.cards];
+        const newCards = [];
+        while (tempCards.length > 0) {
+            const randomIndex = Number.parseInt(Math.random() * (tempCards.length));
+            newCards.push(tempCards.splice(randomIndex, 1)[0]);
+        }
+
+        return newCards;
+    }
+
+    useEffect(() => {
+        const newCards = getCardsRandom();
+        console.log(newCards);
+        setCards(newCards);
+    }, [])
     return (
         <div className="card-manager">
             {cards.map((card) => {
+                console.log(card);
                 return (
                     <Card 
                     id={card.id}
