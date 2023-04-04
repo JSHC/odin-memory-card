24d6c029eab251a11b5c1a616e0cbeb738907436
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-04 07:39:17 +0200

    Add onClick prop to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,7 @@ const Card = (props) => {
     }, []);
 
     return (
-        <div className="card">
+        <div className="card" onClick={props.onClick}>
             {imageLoaded ? 
                 <img className="card-image" src={props.image} alt="props.alt"></img>
             :
