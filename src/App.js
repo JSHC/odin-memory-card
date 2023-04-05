9b76995b2db001c62ffd4bb34a566abe392a8903
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-05 12:23:04 +0200

    Add more cats, adjust CardManager grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import './App.css';
 import CardManager from './components/CardManager';
 import cat1 from './images/amber-kipp-75715CVEJhI-unsplash.jpg'
 import cat2 from './images/milada-vigerova-7E9qvMOsZEM-unsplash.jpg'
+import cat3 from './images/hang-niu-Tn8DLxwuDMA-unsplash.jpg'
+import cat4 from './images/zoe-gayah-jonker-uhnbTZC7N9k-unsplash.jpg'
+import cat5 from './images/mathieu-odin-YeQIAysCP3w-unsplash.jpg'
+import cat6 from './images/timo-volz-mrTydVjg04o-unsplash.jpg'
+import cat7 from './images/sajad-nori-s1puI2BWQzQ-unsplash.jpg'
+import cat8 from './images/linli-xu-C26VKA9BMYQ-unsplash.jpg'
+
 
 function App() {
   const cards = [
@@ -10,7 +17,26 @@ function App() {
     },
     {
       id: 2, image: cat2, alt: 'cat2', text: 'Cat 2'
-    }
+    },
+    {
+      id:3, image: cat3, alt: 'cat3', text: 'Cat 3'
+    },
+    {
+      id: 4, image: cat4, alt: 'cat4', text: 'Cat 4'
+    },
+    {
+      id: 5, image: cat5, alt: 'cat5', text: 'Cat 5'
+    },
+    {
+      id: 6, image: cat6, alt: 'cat6', text: 'Cat 6'
+    },
+    {
+      id: 7, image: cat7, alt: 'cat7', text: 'Cat 7'
+    },
+    {
+      id: 8, image: cat8, alt: 'cat8', text: 'Cat 8'
+    },
+    
   ];
   return (
     <div className="App">
