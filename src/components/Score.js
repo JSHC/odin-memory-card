95205b625c73c103f91971d387cf45d0cafd0b30
JSHC <johan.hansson.carlberg@gmail.com>
2023-04-11 09:34:40 +0200

    Add scoreboard styling

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,8 +3,10 @@ import '../styles/Score.css';
 const Score = (props) => {
     return (
         <div className="score">
-            <span className="current-score">Current score: {props.currentScore}</span>
-            <span className="high-score">High score: {props.highScore}</span>
+            <div class="score-text">
+                <span className="current-score">Current score: {props.currentScore}</span>
+                <span className="high-score">High score: {props.highScore}</span>
+            </div>
         </div>
     )
 }
